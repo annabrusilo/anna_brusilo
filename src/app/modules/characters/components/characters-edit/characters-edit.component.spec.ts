e97460b038ec514c6b184c48b6eb4e2adaf58f83
Anna Brusiło <anna.brusilo@e-qsg.com>
2019-06-09 16:45:50 +0200

    fix current tests

--- src/app/modules/characters/components/characters-edit/characters-edit.component.spec.ts
+++ src/app/modules/characters/components/characters-edit/characters-edit.component.spec.ts
@@ -1,14 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CharactersEditComponent } from './characters-edit.component';
+import {CharactersEditComponent} from './characters-edit.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {CharactersFacadeService} from '../../services/characters-facade.service';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('CharactersEditComponent', () => {
   let component: CharactersEditComponent;
   let fixture: ComponentFixture<CharactersEditComponent>;
 
+  const mockedFacadeService = {
+    setLoadListParams(): void {},
+    loadSpecies(): void {},
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CharactersEditComponent ]
+      declarations: [ CharactersEditComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [RouterTestingModule],
+      providers: [{provide: CharactersFacadeService, useValue: mockedFacadeService}]
     })
     .compileComponents();
   }));
--- src/app/modules/characters/components/characters-list/characters-list.component.spec.ts
+++ src/app/modules/characters/components/characters-list/characters-list.component.spec.ts
@@ -1,16 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CharactersListComponent } from './characters-list.component';
+import {CharactersListComponent} from './characters-list.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {CharactersFacadeService} from '../../services/characters-facade.service';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('CharactersListComponent', () => {
   let component: CharactersListComponent;
   let fixture: ComponentFixture<CharactersListComponent>;
 
+  const mockedFacadeService = {
+  };
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CharactersListComponent ]
+      declarations: [CharactersListComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [RouterTestingModule],
+      providers: [{provide: CharactersFacadeService, useValue: mockedFacadeService}],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
